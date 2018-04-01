5a9a7578eafc7a868dc351dfe3931855c0ef84b3
Andrea Kleinschmidt <ak81@oscilloworld.de>
2018-04-01 16:29:17 +0200

    html default template: header geändert + css, textkorrektur readme

--- version.php
+++ version.php
@@ -1,5 +1,5 @@
 <?php
 // This file is part of 'backend by andrea'
 // CMS & blog software with frontend / backend
-define("VERSION","v2018-12-gd290db4");
+define("VERSION","v2018-13-gab6e346");
 ?>
