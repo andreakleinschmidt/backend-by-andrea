32b2685c6d9b632c6f3d5cc930e3ed8b0be37d50
Andrea Kleinschmidt <ak81@oscilloworld.de>
2020-12-17 19:08:07 +0100

    css: bugfix letzter commit (sticky vs align-self)

--- version.php
+++ version.php
@@ -1,5 +1,5 @@
 <?php
 // This file is part of 'backend by andrea'
 // CMS & blog software with frontend / backend
-define("VERSION","v2018-42-g72e448a");
+define("VERSION","v2018-43-gae305e0");
 ?>
